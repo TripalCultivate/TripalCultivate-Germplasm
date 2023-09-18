4fccd9e8a3e124b4a2e927d3aa74a8ef11a2fc3b
Carolyn Caron <carolyn.caron@usask.ca>
2023-09-18 17:05:37 -0600

    Finished designing the run function

--- trpcultivate_germplasm/src/Plugin/TripalImporter/GermplasmAccessionImporter.php
+++ trpcultivate_germplasm/src/Plugin/TripalImporter/GermplasmAccessionImporter.php
@@ -16,19 +16,19 @@ use Drupal\tripal_chado\TripalImporter\ChadoImporterBase;
  *   use_analysis = FALSE,
  *   require_analysis = FALSE,
  *   upload_title = "Germplasm Accession Import",
- *   upload_description = "Germplasm file should be a tab separated file with the following colums:<ol>
+ *   upload_description = "Germplasm file should be a tab separated file with the following columns:<ol>
      <li>Germplasm Name: Name of this germplasm accession.</li>
-     <li>External Database: The institution who assigned the following accession.</li>
+     <li>External Database: The institution who assigned the accession.</li>
      <li>Accession Number: A unique identifier for the accession. </li>
      <li>Germplasm Species: The species of the accession.</li>
-     <li>Germplasm Subtaxa: Subtaxon can be used to store any additional taxonomic identifier.</li>
-     <li>Institute Code: The code for the Institute that has bred the material.</li>
-     <li>Institute Name: The name of the Institute that has bred the material.</li>
-     <li>Country of Origin Code: 3-letter ISO 3166-1 code of the country in which the sample was originally.</li>
+     <li>Germplasm Subtaxa: Subtaxon can be specified here or any additional taxonomic identifier.</li>
+     <li>Institute Code: The code for the Institute that bred the material.</li>
+     <li>Institute Name: The name of the Institute that bred the material.</li>
+     <li>Country of Origin Code: 3-letter ISO 3166-1 code of the country in which the sample was originally sourced.</li>
      <li>Biological Status of Accession: The 3 digit code representing the biological status of the accession.</li>
      <li>Breeding Method: The unique identifier for the breeding method used to create this germplasm.</li>
      <li>Pedigree: The cross name and optional selection history.</li>
-     <li>Synonyms: The synonyms of the accession.</li>
+     <li>Synonyms: Any synonyms of the accession.</li>
      </ol>",
  *   button_text = "Import Germplasm Accessions",
  *   file_upload = True,
@@ -76,17 +76,24 @@ class GermplasmAccessionImporter extends ChadoImporterBase {
    * @see TripalImporter::formValidate()
    */
   public function formValidate($form, &$form_state){
-
+    // Nothing to validate since the genus field is set to "required".
   }
 
   /**
    * @see TripalImporter::run()
    */
   public function run(){
+    // All values provided by the user in the Importer's form widgets are
+    // made available to us here by the Class' arguments member variable.
     $arguments = $this->arguments['run_args'];
 
-    // Grab the file path
+    // The path to the uploaded file is always made available using the
+    // 'files' argument. The importer can support multiple files, therefore
+    // this is an array of files, where each has a 'file_path' key specifying
+    // where the file is located on the server.
     $file_path = $arguments['files'][0]['file_path'];
+
+    // Grab the genus name
     $genus_name = $arguments['genus_name'];
 
     // Set up the ability to track progress so we can report it to the user
@@ -100,18 +107,81 @@ class GermplasmAccessionImporter extends ChadoImporterBase {
     while (!feof($GERMPLASM_FILE)){
       $current_line = fgetcsv($GERMPLASM_FILE, 0, "\t");
 
+      // Calculate how many bytes we have read from the file and let the
+      // importer know how many have been processed so it can provide a
+      // progress indicator.
+      $bytes_read += drupal_strlen($current_line);
+      $this->setItemsHandled($bytes_read);
+
       // Check for empty lines, comment lines and a header line
       if (empty($current_line)) continue;
       if (preg_match('/^#/', $current_line)) continue;
       if (preg_match('/^Germplasm/', $current_line)) continue;
+
+      // Trim the current line for trailing whitespace and split columns
+      // into an array
+      $current_line = trim($current_line);
+      $germplasm_columns = explode("\t", $current_line);
+
+      // Collect our values from our current line into variables
+      $germplasm_name = $germplasm_columns[0];
+      $external_database = $germplasm_columns[1];
+      $accession_number = $germplasm_columns[2];
+      $germplasm_species = $germplasm_columns[3];
+      $germplasm_subtaxa = $germplasm_columns[4];
+      $institute_code = $germplasm_columns[5];
+      $institute_name = $germplasm_columns[6];
+      $country_of_origin_code = $germplasm_columns[7];
+      $biological_status_of_accession_code = $germplasm_columns[8];
+      $breeding_method_DbId = $germplasm_columns[9];
+      $pedigree = $germplasm_columns[10];
+      $synonyms = $germplasm_columns[11];
+
+      // STEP 1: Pull out the organism ID for the current germplasm
+      $organism_ID = $this->getOrganismID($genus_name, $germplasm_species, $germplasm_subtaxa);
+
+      // STEP 2: Check/Insert this germplasm into the Chado stock table
+      $stock_id = $this->getStockID($germplasm_name, $accession_number, $organism_ID);
+
+      // STEP 3: If $external_database is provided, then
+      // Load the external database info into Chado dbxref table
+
+      // STEP 4: Load stock properties (if provided) for:
+      // $institute_code
+      // $institute_name
+      // $country_of_origin_code
+      // $biological_status_of_accession_code
+      // $breeding_method_DbId
+      // $pedigree
+
+      // STEP 5: Load synonyms 
+
     }
+  }
+
+  /**
+   * Checks if an organism exists in Chado and returns the primary key, 
+   * otherwise throws an error if the organism does not exist
+   * 
+   */
+  public function getOrganismID() {
+    return 0;
+  }
+
+  /**
+   * Checks if an stock exists in Chado, inserts it and returns the primary
+   * key in the stock table
+   *
+   */
+  public function getStockID() {
+    return 0;
   }
 
   /*
    * {@inheritdoc}
    */
   public function postRun() {
-
+    // Nothing to clean up.
   }
 
   /**
@@ -120,6 +190,4 @@ class GermplasmAccessionImporter extends ChadoImporterBase {
   public function formSubmit($form, &$form_state) {
 
   }
-
-
 }
